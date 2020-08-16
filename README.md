56dd4045ee58a0c9c7c351f13dd9e4211905d9a4
Simon Larsson <snigelson@gmail.com>
2020-08-16 08:58:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+## Tumblrator - download tumblr feeds
+
+This script gets feeds from Feedly and saves it as a json. It also 
+extracts URLs pointing to tumblrs CDN (*.media.tumblr.com) and tries to 
+download those.
+
+
+## Requirements
+
+Python3
+
+
+## Usage
+
+Run `python tumblrator.py <feed name>`. You can supply any number of 
+feeds in one go, and they will be downloaded sequentially.
+
+Example: `python tumblrator.py probertson`
+
+A drirectory named "<feed name>" should be created. In that direcory, the feed will be saved as `feed.json` along with any (see below) media files.
+
+
+## Caveats
+
+1. I have not bug tested this software like at all.
+
+2. Only media stored on tumblr's CDN will be downloaded.
+External media will not be downloaded.
+
+3. I have only yet tested this software on Linux.
+
+4. No pretty formatting is done. It would probably be easy for someone
+with the know-how to parse the JSON into a nice HTML presentation.
+
+5. I am not sure it extracts all media. If it doesn't, please create
+an issue where you supply the name of the feed and the media which
+is not downloaded. Thank you!
---
+++ tumblrator.py
@@ -0,0 +1,58 @@
+import urllib.request
+import json
+import sys
+import os
+import re
+
+def get_feed(user):
+	url = "http://cloud.feedly.com/v3/streams/contents?streamId=feed/http://"
+	url+= user
+	url+= ".tumblr.com/rss&ranked=oldest"
+	handle = urllib.request.urlopen(url)
+	feed = handle.read()
+	return feed
+
+def get_media(url, savepath='.'):
+	media_basename = url.split("/")[-1]
+	print("=== Downloading {}".format(media_basename))
+	urllib.request.urlretrieve(url, os.path.join(savepath,media_basename))
+
+if __name__ == "__main__":
+
+	#	feed_json = get_feed("probertson")
+	#	feed_json = get_feed("exposedtease")
+
+	feed_list = sys.argv[1:]
+
+	for f_name in feed_list:
+		print("Downloading feed: {}".format(f_name))
+		try:
+			os.mkdir(f_name)
+		except FileExistsError:
+			if not os.path.isdir(f_name):
+				print("ERROR: Could not create directory {}".format(f_name))
+				continue
+			else:
+				print("Directory {} already exists.".format(f_name))
+
+		feed_json = get_feed(f_name)
+		feed = json.loads(feed_json)
+		with open(os.path.join(f_name, "feed.json"),"w") as f:
+			f.write(json.dumps(feed, indent=4, sort_keys=True))
+
+		print("Downloading media...")
+
+		media = []
+		for post in feed["items"]:
+			if "visual" in post:
+				visual_url = post["visual"]["url"]
+				if "media.tumblr.com" in visual_url:
+					if not visual_url in media:
+						media.append(visual_url)
+
+			for url in re.findall("http://[0-9]+.media.tumblr.com/[^\"]+",post["summary"]["content"]):
+				if not url in media:
+					media.append(url)
+
+		for url in media:
+			get_media(url, savepath=f_name)
